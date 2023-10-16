dd3ce2a73c49a9b64add0d4787bed260c20b912f
danik0n <gviner52@gmail.com>
2023-10-16 14:50:59 +0300

    Start the game hehehaha

---
+++ src/components/UI/index.js
@@ -0,0 +1,15 @@
+import AnonimusModal from "@/components/UI/AnonimusModal";
+import HomeButton from "@/components/UI/HomeButton";
+import MainButton from "@/components/UI/MainButton";
+import MenuButton from "@/components/UI/MenuButton";
+import SecondaryButton from "@/components/UI/SecondaryButton";
+import WhiteButton from "@/components/UI/WhiteButton";
+
+export default [
+    AnonimusModal,
+    HomeButton,
+    MainButton,
+    MenuButton,
+    SecondaryButton,
+    WhiteButton,
+]
